f10f96ba8ed16ba55a6c535c120e2a8f6bea2204
Peter Zastoupil <peterzastoupil@gmail.com>
2025-11-29 01:18:35 -0600

    feature: add minimal remote control handlers

--- apps/ui/src/app/playbackService.ts
+++ apps/ui/src/app/playbackService.ts
@@ -1,1 +1,20 @@
-export default async function () {}
+import TrackPlayer, { Event } from 'react-native-track-player'
+export default async function () {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.setVolume(1)
+    TrackPlayer.play()
+  })
+  TrackPlayer.addEventListener(Event.RemotePause, () =>
+    TrackPlayer.setVolume(0)
+  )
+  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.setVolume(0))
+  TrackPlayer.addEventListener(Event.RemoteDuck, ({ paused, permanent }) => {
+    if (permanent) {
+      TrackPlayer.setVolume(0)
+    } else if (paused) {
+      TrackPlayer.setVolume(0.5)
+    } else {
+      TrackPlayer.setVolume(1)
+    }
+  })
+}
