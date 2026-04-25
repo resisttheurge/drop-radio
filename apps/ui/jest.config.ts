3dbe1588c075e896b21bab1f2c0e8667548efde6
Peter Zastoupil <peterzastoupil@gmail.com>
2026-04-25 13:35:57 -0500

    gen: generate initial expo ui app

---
+++ apps/ui/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@drop-radio/ui',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'test-output/jest/coverage',
+}
