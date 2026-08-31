3392496bbd74adcb47da206d16064d74b227a287
Peter Zastoupil <peterzastoupil@gmail.com>
2026-08-31 08:25:18 -0500

    feat: test video visualizer

--- apps/ui/src/components/stream-player/control.tsx
+++ apps/ui/src/components/stream-player/control.tsx
@@ -1,10 +1,62 @@
-import { AudioPlayer } from 'expo-audio'
-import { View } from 'react-native'
+import Icon from '@expo/vector-icons/Ionicons'
+import { useMemo } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import Neu from '../neu'
+import { Status } from './status'
 
-export interface ControlProps {
-  player: AudioPlayer
+export interface StreamControlProps {
+  lightAngle: number
+  active: boolean
+  status: Status
+  onPress: () => void
 }
 
-export default function Control({ player }: ControlProps) {
-  return <View></View>
+export default function StreamControl({
+  lightAngle,
+  active,
+  status,
+  onPress,
+}: StreamControlProps) {
+  const icon = useMemo(() => {
+    switch (status) {
+      case 'playing':
+        return <Icon size={15} name="pause" />
+      case 'paused':
+        return <Icon size={15} name="play" />
+      case 'waiting':
+        return <Icon size={15} name="refresh" />
+    }
+  }, [status])
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        styles.button,
+        active ? styles.buttonActive : styles.buttonDormant,
+      ]}
+      onPress={onPress}
+    >
+      <Neu angle={lightAngle} intensity={15} curvature="convex" slant extrude>
+        {icon}
+      </Neu>
+    </TouchableOpacity>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    transitionProperty: 'opacity',
+    transitionDuration: '200ms',
+  },
+  buttonDormant: {
+    opacity: 0.15,
+  },
+  buttonActive: {
+    opacity: 0.75,
+  },
+})
--- apps/ui/src/components/stream-player/player.tsx
+++ apps/ui/src/components/stream-player/player.tsx
@@ -1,16 +1,105 @@
-import { useAudioPlayer } from 'expo-audio'
-import { View } from 'react-native'
+import useStream from '@/hooks/useStream'
+import { useVideoPlayer, VideoView } from 'expo-video'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { Pressable, StyleSheet, View } from 'react-native'
+import Neu from '../neu'
 import Control from './control'
-import Visualizer from './visualizer'
+import { Status } from './status'
 
 export default function StreamPlayer() {
-  const player = useAudioPlayer(
-    'https://server.test.drop-radio.info/stream/live.m3u8'
+  const [lightAngle, setLightAngle] = useState(0.66 * Math.PI)
+  const [controlsActive, setControlsActive] = useState(false)
+  const [lastUserInteraction, setLastUserInteraction] = useState(Date.now())
+  useEffect(() => {
+    if (controlsActive) {
+      const timeout = setTimeout(() => {
+        setControlsActive(false)
+      }, 3000)
+      return () => clearTimeout(timeout)
+    }
+  }, [controlsActive, lastUserInteraction])
+
+  const [audioPlayer, audioStatus] = useStream(
+    'https://server.dropradio.org/stream/live.m3u8'
   )
+
+  const status: Status = useMemo(() => {
+    if (audioStatus.playing) {
+      return 'playing'
+    }
+    if (!audioStatus.isBuffering && audioStatus.isLoaded) {
+      return 'paused'
+    }
+    return 'waiting'
+  }, [audioStatus.playing, audioStatus.isBuffering, audioStatus.isLoaded])
+
+  const video = require('../../../assets/movies/the-move.mp4')
+  const videoPlayer = useVideoPlayer(video, (player) => {
+    player.volume = 0
+    player.loop = true
+    player.play()
+  })
+
+  const togglePlay = useCallback(() => {
+    if (audioStatus.playing) {
+      audioPlayer.pause()
+    } else {
+      audioPlayer.play()
+      videoPlayer.play()
+    }
+  }, [audioPlayer, audioStatus, videoPlayer])
+
   return (
-    <View style={{ flex: 1 }}>
-      <Visualizer visualization="spinning-man" player={player} />
-      <Control player={player} />
-    </View>
+    <Neu angle={lightAngle} intensity={2} style={styles.container} slant>
+      <Pressable
+        style={{ flex: 1 }}
+        onPress={() => {
+          console.log('Press')
+          setControlsActive(!controlsActive)
+          setLastUserInteraction(Date.now())
+        }}
+        onHoverIn={() => {
+          console.log('HoverIn')
+          setControlsActive(true)
+          setLastUserInteraction(Date.now())
+        }}
+        onPointerMove={(event) => {
+          console.log('PointerMove')
+          setControlsActive(true)
+          setLastUserInteraction(Date.now())
+        }}
+      >
+        <VideoView
+          style={styles.video}
+          player={videoPlayer}
+          nativeControls={false}
+          contentFit="cover"
+        />
+      </Pressable>
+      <View style={styles.control}>
+        <Control
+          lightAngle={lightAngle}
+          status={status}
+          active={controlsActive}
+          onPress={togglePlay}
+        />
+      </View>
+    </Neu>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  video: {
+    flex: 1,
+    opacity: 0.85,
+  },
+  control: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: '-50%' }, { translateY: '-50%' }],
+  },
+})
