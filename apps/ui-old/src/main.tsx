d1fc22d880fc10a4bd382811fb7f1f18dff08caf
Peter Zastoupil <peterzastoupil@gmail.com>
2026-09-01 14:29:30 -0500

    chore: remove old ui app directories as they will no longer be used

--- apps/ui-old/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native'
-import TrackPlayer from 'react-native-track-player'
-
-import App from './app/App'
-import playbackService from './app/playbackService'
-
-AppRegistry.registerComponent('Ui', () => App)
-TrackPlayer.registerPlaybackService(() => playbackService)
